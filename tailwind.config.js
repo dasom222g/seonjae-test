b60c89c08ed5ebfb0c53cbb3a4f2e45034c1ec91
dasom222g <dasom228@gmail.com>
2023-02-08 22:03:36 +0900

    Set tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        no: '0rem',
+      },
+      colors: {
+        'mbti-yellow': '#FAEF96',
+        'mbti-mint': '#1DCAB6',
+        'mbti-light-mint': '#6EFFEE',
+        'mbti-deeplight-mint': '#C3FCF1',
+        'mbti-brown': '#625F4E',
+        'mbti-light-brown': '#AFAB99',
+        'mbti-light-orange': '#FFCA8D',
+        'mbti-coral': '#F98E71',
+        'mbti-light-coral': '#FECCBE',
+        'mbti-deep-blue': '#645CBB',
+        'mbti-purple': '#FF90DF',
+        'mbti-gray': '#4A4737',
+        'mbti-red': '#ED3B38',
+      },
+    },
   },
   plugins: [],
 }

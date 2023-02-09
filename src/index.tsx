acf466f5eeaed55c4ed8ba8d4daae2fc8be9699e
dasom222g <dasom228@gmail.com>
2023-02-09 19:51:55 +0900

    Set font

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 // style
 import 'tailwindcss/tailwind.css'
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(

24eb4190ce82e863f00183b3ce226e99fd808abc
dasom222g <dasom228@gmail.com>
2024-05-23 17:29:37 +0900

    Fix button style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -14,9 +14,10 @@ const Button: FC<ButtonProps> = ({ data, handleClick }) => {
     <div className={style.roundInterval}>
       <button
         type="button"
-        className={`${style.roundBox} text-white bg-mbti-deep-purple`}
+        className={`flex gap-2 justify-center items-center text-${data.id === 1 ? 'mbti-blue' : 'white'} bg-${data.id === 1 ? 'white' : 'mbti-blue'} font-cafe24surround rounded-4xl border-[6px] border-mbti-blue py-1 w-3/4 mx-auto`}
         onClick={buttonClick}>
         {data.text}
+        <data.icon size={32} />
       </button>
     </div>
   )

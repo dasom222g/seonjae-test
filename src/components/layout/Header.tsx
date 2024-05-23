f18f1cc07c78501bf1556edcf4b5869acdf964a9
dasom222g <dasom228@gmail.com>
2024-05-23 17:29:55 +0900

    Fix sttyle

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Header: FC = () => {
   return (
-    <header className='bg-mbti-deep-purple -m-4 mb-0 px-4 py-3'>
+    <header className='bg-mbti-deep-purple -mx-4 mb-0 px-4 py-3 absolute z-50 w-full max-h-12.5'>
       <Link to="/" className="block">
         <h1 className='text-white text-lg font-cafe24surround'>
           <span className='text-mbti-mint'>Zero</span>ToOne
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -11,11 +11,11 @@ const Layout: FC<LayoutProps> = ({ children }) => {
   }
   return (
     <div
-      className="bg-mbti-yellow bg-repeat min-h-screen h-full p-4 overflow-auto font-pretendard"
+      className="bg-mbti-yellow bg-repeat min-h-screen h-full px-4 overflow-auto font-pretendard"
       style={bgStyle}>
       <div className="h-full flex flex-col">
         <Header />
-        <main className="px-4 h-full w-full max-w-lg mx-auto"> {children}</main>
+        <main className="px-4 h-full w-full max-w-lg mx-auto pt-12.5"> {children}</main>
       </div>
     </div>
   )

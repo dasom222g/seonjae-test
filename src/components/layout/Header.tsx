977423916c81516d9e6a8b58ad39d895a8c6fe57
dasom222g <dasom228@gmail.com>
2024-05-27 15:44:24 +0900

    Fix simantic tag

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 
 const Header: FC = () => {
   return (
-    <header className='bg-mbti-deep-purple -mx-4 mb-0 px-4 py-3 absolute z-50 w-full max-h-12.5'>
-      <Link to="/" className="block">
-        <h1 className='text-white text-lg font-cafe24surround'>
-          <span className='text-mbti-mint'>Zero</span>ToOne
-        </h1>
-      </Link>
+    <header className='bg-mbti-deep-purple -mx-4 mb-0 absolute z-50 w-full max-h-12.5'>
+      <h3 className='text-white text-lg font-cafe24surround'>
+        <Link to="/" className="inline-block  px-4 py-3">
+          <span className='text-mbti-mint'>선재랑 </span>궁합테스트
+        </Link>
+      </h3>
     </header>
   )
 }

c70b5f64c4957ff7d076269be9b3bde4297aa28e
dasom222g <dasom228@gmail.com>
2024-05-28 00:48:26 +0900

    Fix google analytics setting

---
+++ src/lib/RouteChangeTracker.tsx
@@ -0,0 +1,34 @@
+import { FC, useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom';
+import ReactGA from 'react-ga4';
+
+const trackingId = process.env.REACT_APP_GA_TRACKING_ID;
+
+const RouteChangeTracker: FC = () => {
+  const location = useLocation();
+  const [initialized, setInitialized] = useState(false);
+
+  // 진입시
+  useEffect(() => {
+    if (!window.location.href.includes('localhost')) {
+      if (trackingId) {
+        ReactGA.initialize(trackingId);
+        return
+      }
+      console.error('Google Analytics Tracking ID is not defined');
+    }
+    setInitialized(true);
+  }, []);
+
+  // initialized 후
+  useEffect(() => {
+    if (initialized) {
+      ReactGA.set({ page: location.pathname });
+      ReactGA.send('pageview');
+      console.log('initialized', initialized)
+    }
+  }, [initialized, location]);
+  return null;
+}
+
+export default RouteChangeTracker
